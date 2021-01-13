73c0b9f3881363bc3fbe86e5ae79701ca3e4cbba
Matthew Webb <75703308+MWebb2321@users.noreply.github.com>
2021-01-13 11:02:35 -0800

    Add files via upload
    Made progress on the Javascript today, but still not at the point of submission just yet.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # PassGen.github.io
+
 PasswordGenerator
+
+1/7/21
+In this homework assignment, I was tasked with building a working Password Generator. My plan is determine the best course of action in regards to making the application work as requested. Going over the given HTML, I notice that nothing happens after inputting a numerical value. That will soon be fixed.
+
+1/11/21
+To be honest, this assignment is giving me grief. I've been trying to figure out functions and how to possibly get the JavaScript to remember user input, but I cannot figure it out. I'll keep at it though.
+
+1/13/21
+After some help, I was able to get my prompts working, but there is an issue with the end section.
--- script.js
+++ script.js
@@ -1,19 +1,52 @@
 // Assignment Code
-var generateBtn = document.querySelector('#generate');
+var generateBtn = document.querySelector("#generate");
+var charLength = document.querySelector("#pwLength");
+var charUpper = document.querySelector("#charUpper");
+var charLower = document.querySelector("#charLower");
+var charNumber = documen.querySelector("#charNumber");
+var charSpecial = document.querySelector("#charSpecial");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector('#password');
-
+  var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
 
 function generatePassword() {
-  var pwlength = parseInt(prompt('How many chars?'));
-  console.log(typeof pwlength);
-  console.log();
-}
+  var charUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var charLower = "abcdefghijklmnopqrstuvwxyz";
+  var charNumber = "1234567890";
+  var charSpecial = "!@#$%^&*()><}{][";
+
+  var pwLength = parseInt(prompt("How many characters? Between 8 and 128"));
+
+    if (pwLength < 8 || pwLength > 128) {
+      alert("This needs a proper value, please and thank you!");
+    } 
+    else {
+      alert("This is a good number.");
+    }
+  
+  var charUpper = confirm("Include upper case?");{
+    console.log(charUpper)
+  }
+
+  var charLower = confirm("Include lower case?");{
+    console.log(charLower)
+  }
+
+  var charNumber = confirm("Include numbers?");{
+    for (var i = 0; i < length; i++){
+    }
+    console.log(charNumber)
+  }
+
+  var charSpecial = confirm("Include special characters?");{
+    console.log(charSpecial)
+  }
 
 // Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+  generateBtn.addEventListener('click', writePassword);{
+    
+  }
